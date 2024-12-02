b5d0585c0951087b982e15c2a7b4da0a89ebe755
971124641@qq.com <email>
2024-12-02 08:11:04 +0800

    feat: 更换supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://upwupaixeibkffuhkisg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwd3VwYWl4ZWlia2ZmdWhraXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5MzMxMTAsImV4cCI6MjA0NzUwOTExMH0.PH43er5FqXqil2qKTUZzZ_FsstwtWJ9rEGchI3x-9iw')
+const supabase = createClient('https://qtbqzufcvvaqaadcqsbj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0YnF6dWZjdnZhcWFhZGNxc2JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNjg2NTEsImV4cCI6MjA0ODY0NDY1MX0.V_3fnZuMaICuShj5z2gpgy_ZzOm6jDWomJhNqwaMJkE')
 
 export default supabase;
